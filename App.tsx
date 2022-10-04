e937d2d617d7c67b6001c1847e36c2ad2b0135f8
darthpaul <paulakhamiogu@gmail.com>
2022-10-04 07:51:21 +0100

    [CHORE]: Initial project setup - setup ESLint - setup Prettier - setup basic bottom navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import useCachedResources from "./src/hooks/useCachedResources";
+import useColorScheme from "./src/hooks/useColorScheme";
+import Navigation from "./src/navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+            </SafeAreaProvider>
+        );
+    }
+}
---
+++ src/components/StyledText.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import { Text, TextProps } from "./Themed";
+
+export function MonoText(props: TextProps) {
+    return (
+        <Text
+            {...props}
+            style={[props.style, { fontFamily: "DMSans_400Regular" }]}
+        />
+    );
+}
